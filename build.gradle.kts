26cb1341cc87e24ed8e5295c4770d80bb20bd8ff
Juan Gerardo <juange.mendez.lopez@gmail.com>
2025-12-30 13:51:26 -0500

    fix: gradle project reactor dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,19 +10,24 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("io.projectreactor:reactor-bom:2024.0.5"))
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation ("io.projectreactor:reactor-core")
-    implementation ("io.projectreactor.netty:reactor-netty-core")
-    implementation ("io.projectreactor.netty:reactor-netty-http")
+
+    implementation("io.projectreactor:reactor-core")
+    implementation("io.projectreactor.netty:reactor-netty-core")
+    implementation("io.projectreactor.netty:reactor-netty-http")
     implementation("org.reactivestreams:reactive-streams:1.0.4")
-    testImplementation ("io.projectreactor:reactor-test")
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+
     implementation("org.slf4j:slf4j-api:2.0.13")
     runtimeOnly("ch.qos.logback:logback-classic:1.5.13")
+
     implementation("com.github.javafaker:javafaker:1.0.2")
-    testImplementation ("io.projectreactor:reactor-test")
 }
 
 tasks.test {
